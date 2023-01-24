aa75a7091afd86d46f3eea98a58b8f9a95281e0a
Konstantin Pivnov <vaproloff@ya.ru>
2023-01-24 21:53:07 +0300

    added seminar 2 homework exercise 2 solution

--- src/main/java/sem_02_hw/ex2.java
+++ src/main/java/sem_02_hw/ex2.java
@@ -1,10 +1,50 @@
 package sem_02_hw;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 /*  Напишите метод, который составит строку, состоящую из 100 повторений слова TEST и метод,
     который запишет эту строку в простой текстовый файл, обработайте исключения.
 */
 public class ex2 {
     public static void main(String[] args) {
+        Scanner lineScanner = new Scanner(System.in);
 
+        String word = "";
+        do {
+            System.out.print("Введите слово: ");
+            word = lineScanner.nextLine();
+        } while (word.isBlank());
+
+        int repeats = 0;
+        do {
+            System.out.print("Введите количество повторений: ");
+            try {
+                repeats = Integer.parseInt(lineScanner.next());
+                if (repeats <= 0) {
+                    System.out.println("Это не положительное число");
+                }
+            } catch (Exception e) {
+                System.out.println("Ошибка! Неверно введено число.");
+            }
+        } while (repeats <= 0);
+
+        lineScanner.close();
+
+        String filePath = "src/main/java/sem_02_hw/file.txt";
+        printStringToFile(multiplyString(word, repeats), filePath);
+    }
+
+    private static String multiplyString(String inputStr, int repeats) {
+        return inputStr.repeat(Math.max(0, repeats));
+    }
+
+    private static void printStringToFile(String outputStr, String filePath) {
+        try(PrintWriter pw = new PrintWriter(filePath)) {
+            pw.print(outputStr);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
