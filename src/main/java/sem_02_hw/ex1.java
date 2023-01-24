37367c416995ca4e7349d3c36b0bf2cbaa0600f5
Konstantin Pivnov <vaproloff@ya.ru>
2023-01-24 21:09:07 +0300

    added seminar 2 homework exercise 1 solution

--- src/main/java/sem_02_hw/ex1.java
+++ src/main/java/sem_02_hw/ex1.java
@@ -1,10 +1,26 @@
 package sem_02_hw;
 
+import java.util.Scanner;
+
 /*  Напишите метод, который принимает на вход строку (String)
     и определяет является ли строка палиндромом (возвращает boolean значение).
 */
 public class ex1 {
     public static void main(String[] args) {
+        Scanner lineScanner = new Scanner(System.in);
+        String inputStr = "";
+        do {
+            System.out.print("Введите строку: ");
+            inputStr = lineScanner.nextLine();
+        } while (inputStr.isBlank());
+        lineScanner.close();
 
+        System.out.println(checkPalindrom(inputStr) ? "Это палиндром" : "Это не палиндром");
+    }
+
+    private static boolean checkPalindrom(String inputStr) {
+        StringBuilder line = new StringBuilder();
+        line.append(inputStr);
+        return line.toString().equals(line.reverse().toString());
     }
 }
