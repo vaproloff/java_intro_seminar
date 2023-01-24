03b3bd4e3060fba1bec556a35af0d44739a904da
Konstantin Pivnov <vaproloff@ya.ru>
2023-01-24 14:07:02 +0300

    initiated package for seminar 2 homework tasks

---
+++ src/main/java/sem_02_hw/ex1.java
@@ -0,0 +1,10 @@
+package sem_02_hw;
+
+/*  Напишите метод, который принимает на вход строку (String)
+    и определяет является ли строка палиндромом (возвращает boolean значение).
+*/
+public class ex1 {
+    public static void main(String[] args) {
+
+    }
+}
