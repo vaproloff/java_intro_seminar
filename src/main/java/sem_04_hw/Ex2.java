24cd08b3cd789bf84339fe7fa794980a623fcfd8
Konstantin Pivnov <vaproloff@ya.ru>
2023-02-06 00:16:35 +0300

    added two more solutions for seminar 4 exercise 2

--- src/main/java/sem_04_hw/Ex2.java
+++ src/main/java/sem_04_hw/Ex2.java
@@ -13,10 +13,31 @@ public class Ex2 {
             numbers.add(i + 1);
         }
         System.out.println("Исходный список: " + numbers);
-        System.out.println("Перевёрнутый список: " + reverseNumbers(numbers));
+        System.out.println("Перевёрнутый список (способ 1): " + reverseNumbers1(numbers));
+        System.out.println("Перевёрнутый список (способ 2): " + reverseNumbers2(numbers));
+        System.out.println("Перевёрнутый список (способ 3): " + reverseNumbers3(numbers));
     }
 
-    private static List<Integer> reverseNumbers(List<Integer> numbers) {
+    private static List<Integer> reverseNumbers1(List<Integer> numbers) {
+        List<Integer> reversedNumbers = new LinkedList<>();
+        for (int i = numbers.size() - 1; i >= 0 ; i--) {
+            reversedNumbers.add(numbers.get(i));
+        }
+        return reversedNumbers;
+    }
+
+    private static List<Integer> reverseNumbers2(List<Integer> numbers) {
+//        Создаю копию только для задания, чтобы третий способ работал с исходным списком
+        List<Integer> reversedNumbers = new LinkedList<>(numbers);
+        for (int i = 0; i < reversedNumbers.size() / 2; i++) {
+            int temp = reversedNumbers.get(i);
+            reversedNumbers.set(i, reversedNumbers.get(reversedNumbers.size() - i - 1));
+            reversedNumbers.set(reversedNumbers.size() - i - 1, temp);
+        }
+        return reversedNumbers;
+    }
+
+    private static List<Integer> reverseNumbers3(List<Integer> numbers) {
         List<Integer> reversedNumbers = new LinkedList<>();
         while (!numbers.isEmpty()) {
             reversedNumbers.add(0, numbers.remove(0));
