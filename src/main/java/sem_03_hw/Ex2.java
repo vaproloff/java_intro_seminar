4505f3ca70d705e5d69c802d7f31ffe978d2f588
Konstantin Pivnov <vaproloff@ya.ru>
2023-02-05 23:35:48 +0300

    added some enhancements into seminar 2 exercise 2 and 3 solutions

--- src/main/java/sem_03_hw/Ex2.java
+++ src/main/java/sem_03_hw/Ex2.java
@@ -2,6 +2,8 @@ package sem_03_hw;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.IntSummaryStatistics;
+import java.util.List;
 import java.util.Random;
 
 /*  Задан целочисленный список ArrayList.
@@ -9,20 +11,25 @@ import java.util.Random;
 */
 public class Ex2 {
     public static void main(String[] args) {
-        ArrayList<Integer> numList = new ArrayList<>();
+        List<Integer> numbers = new ArrayList<>();
         Random random = new Random();
         for (int i = 0; i < 100; i++) {
-            numList.add(random.nextInt(1000));
+            numbers.add(random.nextInt(1000));
         }
         System.out.println("Список:");
-        System.out.println(numList);
+        System.out.println(numbers);
 
+        printStats1(numbers);
+        printStats2(numbers);
+    }
+
+    private static void printStats1(List<Integer> numList) {
         System.out.printf("Минимальное значение: %d%n", Collections.min(numList));
         System.out.printf("Максимальное значение: %d%n", Collections.max(numList));
         System.out.printf("Среднее арифметическое: %f%n", calculateAverage(numList));
     }
 
-    private static double calculateAverage(ArrayList<Integer> numList) {
+    private static double calculateAverage(List<Integer> numList) {
         Integer sum = 0;
         if (!numList.isEmpty()) {
             for (Integer num: numList) {
@@ -31,5 +38,12 @@ public class Ex2 {
             return sum.doubleValue() / numList.size();
         }
         return sum;
+    }
+
+    private static void printStats2(List<Integer> numList) {
+        IntSummaryStatistics stats = numList.stream().mapToInt(i -> i).summaryStatistics();
+        System.out.printf("min: %d%n", stats.getMin());
+        System.out.printf("max: %d%n", stats.getMax());
+        System.out.printf("avg: %f%n", stats.getAverage());
     }
 }
